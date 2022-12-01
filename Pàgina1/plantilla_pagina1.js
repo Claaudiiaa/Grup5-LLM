cd172f230256514dba924096cc2be24b44434302
BitterKass <ariacasellas@inspladelestany.cat>
2022-12-01 16:19:01 +0100

    afegin footer al fitxer javascript

---
+++ Pàgina1/plantilla_pagina1.js
@@ -0,0 +1,14 @@
+var header = document.getElementById("footer1");
+header.innerHTML = `
+<footer class="text-center text-white fourth-color">
+<!-- Grid container -->
+<div class="container p-4"></div>
+<!-- Grid container -->
+<!-- Copyright -->
+<div class="text-center p-3">
+  Arià Caselles i Clàudia Fariña
+</div>
+<!-- Copyright -->
+</footer>
+
+`;
