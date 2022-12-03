7f9f24afc2ca00db9d5b92f3b85efa9b59c03f5a
BitterKass <ariacasellas@inspladelestany.cat>
2022-12-03 16:07:49 +0100

    afegint plantilles de header i footer

---
+++ plantilla_pagina1.js
@@ -0,0 +1,25 @@
+var header = document.getElementById("header");
+header.innerHTML = `
+<header>
+<div class="p-5 text-center bg-image" style="
+    background-image: url('https://img.remediosdigitales.com/48577f/mallard-4414758_1280/1366_521.jpg');
+    ">
+  <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+    <div class="d-flex justify-content-center align-items-center h-100">
+      <div class="text-white">
+        <h1 class="mb-3">Patos uwu</h1>
+      </div>
+    </div>
+  </div>
+</div>
+</header>
+`;
+var footer = document.getElementById("footer");
+footer.innerHTML = `
+<footer class="text-center text-white fourth-color">
+<div class="container p-4"></div>
+<div class="text-center p-3">
+  Arià Caselles i Clàudia Fariña
+</div>
+</footer>
+`;
