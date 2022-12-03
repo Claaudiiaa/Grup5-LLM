af31467d400162f295e24b53544775d9acb47eeb
BitterKass <ariacasellas@inspladelestany.cat>
2022-12-03 16:25:47 +0100

    canviant el footer

--- plantilla_pagina1.js
+++
@@ -1,9 +0,0 @@
-var footer = document.getElementById("footer");
-footer.innerHTML = `
-<footer class="text-center text-white fourth-color">
-<div class="container p-4"></div>
-<div class="text-center p-3">
-  Arià Caselles i Clàudia Fariña
-</div>
-</footer>
-`;
