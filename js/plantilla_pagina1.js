8079c687e6d608ab175166c3849850a2cc1f08b6
claaudiiaa <claudiafarina@inspladelestany.cat>
2022-12-04 11:39:12 +0100

    Actualitzant la pagina

--- js/plantilla_pagina1.js
+++ js/plantilla_pagina1.js
@@ -9,20 +9,8 @@ footer.innerHTML = `
              class="d-flex justify-content-between p-4"
              style="background-color: #6351ce"
              >
-      <!-- Left -->
-      <div class="me-5">
-        <span>Get connected with us on social networks:</span>
-      </div>
-      <!-- Left -->
-
-      <!-- Right -->
-      <div>
-        <a href="" class="text-white me-4">
-          <i class="fab fa-facebook-f"></i>
-        </a>
       
-      </div>
-      <!-- Right -->
+      
     </section>
     <!-- Section: Social media -->
 
@@ -46,133 +34,23 @@ footer.innerHTML = `
           </div>
           <!-- Grid column -->
 
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold">Useful links</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <p>
-              <a href="#!" class="text-white">Your Account</a>
-            </p>
-            <p>
-              <a href="#!" class="text-white">Become an Affiliate</a>
-            </p>
-            <p>
-              <a href="#!" class="text-white">Shipping Rates</a>
-            </p>
-            <p>
-              <a href="#!" class="text-white">Help</a>
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold">Contact</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-            <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-            <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-            <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-          </div>
-          <!-- Grid column -->
         </div>
         <!-- Grid row -->
       </div>
     </section>
     <!-- Section: Links  -->
 
-    <!-- Copyright -->
+    <!-- Github -->
     <div
          class="text-center p-3"
          style="background-color: rgba(0, 0, 0, 0.2)"
          >
-      © 2020 Copyright:
-      <a class="text-white" href="https://github.com/Claaudiiaa/Grup5-LLM"
-         >Grup5</a
-        >
-    </div>
-    <!-- Copyright -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+  </svg> Github:
+ <a class="text-white" href="https://github.com/Claaudiiaa/Grup5-LLM.git"
+    >Grup5</a
+   ></div>
+   <!-- Github -->
   </footer>
-`;
-var content = document.getElementById("anecs");
-content.innerHTML = `
-<content>
-<div class="row">
-  <div class="col col-6 m-4">
-    L'ànec o ànet és el nom vulgar que reben un bon nombre d'espècies d'ocells de la família dels anàtids
-    (Anatidae). Aquests ocells estan repartits entre diferents subfamílies, no representant un grup monofilètic. Els
-    ànecs són ocells principalment aquàtics, normalment més petits que cignes i oques, i es poden trobar principalment
-    en aigua dolça, però també a la mar. <br><br>
-
-    <strong>Etimologia i terminologia</strong><br>
-    La paraula «ànec» deriva del llatí anate (amb el mateix significat) a través de la variant més antiga ànet. La
-    femella de l'ànec o ànet rep el nom d'ànega o àneda. Hi ha variants com ara àneda o ànnera que poden referir-se a
-    l'espècie en general, sense una marca de gènere femenina. Així, hi ha l'àneda o ànnera mascle i àneda o ànnera
-    femella. La cria és un anedó. Tiró és una denominació d'origen expressiu per a denominar sobretot l'ànec
-    casolà. Alguns d'aquests ocells reben altres noms particulars, com ara «xarxet», «morell» o «bec de serra,»
-    però són considerats en general ànecs. <br>
-  </div>
-  <div class="col col-5 m-4">
-    <img src="../Imatges/anecs.jpg" height="150px">      
-  </div>
-</div>
-</content>
-`;
-var contingut = document.getElementById("gats");
-contingut.innerHTML = `
-<content>
-  <div class="row">
-    <div class="col col-6 m-4">
-      El gat o moix (Felis catus) és un petit mamífer carnívor domèstic. Es tracta de l'única espècie domesticada de
-      la família dels fèlids i sovint és conegut com a gat domèstic per distingir-lo dels gats salvatges. Els gats
-      poden ser casolans, de granja o ferals; aquests últims viuen en llibertat i eviten el contacte amb els humans.
-      Els éssers humans tenen gats com a animals domèstics i per mantenir a ratlla els rosegadors i altres
-      plagues.<br><br>
-
-      La seva anatomia s'assembla força a la de les altres espècies de fèlids, amb el cos fort i flexible, reflexos
-      molt ràpids, dents afilades i urpes retràctils adaptades per matar preses de petites dimensions. Tenen la visió
-      nocturna i el sentit de l'olfacte ben desenvolupats. Es comuniquen mitjançant vocalitzacions que inclouen miols,
-      roncs, esbufecs i grunyits, així com el llenguatge corporal. Malgrat que cacen en solitari, són animals socials.
-      Perceben sons massa fluixos o aguts per a l'orella humana, com ara els sorolls que fan els ratolins i altres
-      mamífers petits. Habitualment surten a caçar durant el crepuscle.<br>
-    </div>
-    <div class="col col-5 m-4">
-      <img src="../Imatges/gaticos.jpg" height="400px">
-    </div>
-  </div>
-</content>
-`;
-var content = document.getElementById("axolot");
-content.innerHTML = `
-<content>
-  <div class="row">
-    <div class="col col-6 m-4">
-      L'axolot (Ambystoma mexicanum) conegut en idioma nàhuatl com a axolot, que vol dir "monstre aquàtic" és un
-      amfibi urodel que viu al llac Xochimilco i altres llacs de Mèxic, i també a molts aquaris de tot el món. A
-      Mèxic ha estat tradicionalment un producte comestible.
-      <br><br>
-
-      <strong>Biologia</strong><br>
-      A primer cop d'ull el seu estrany aspecte recorda el d'un cap gros gegant. El seu cos és allargat i el cap gran
-      amb els ulls petits. A l'altura del coll té tres parells de brànquies laterals externes. De tant en tant puja a
-      la superfície per a respirar també l'aire atmosfèric.
-
-      El seu metabolisme és molt peculiar, ja que és un cas de neotènia com també tenen altres urodels mexicans del
-      gènere Ambystoma. L'axolotl ha desenvolupat la capacitat de madurar sexualment i reproduir-se conservant l'estat
-      de larva o la majoria dels seus aspectes. També té la capacitat de regenerar parts del cos perdudes, per això
-      l'axolot és considerada una de les salamandres més rares del món. <br>
-    </div>
-    <div class="col col-5 m-4">
-      <img src="../Imatges/ajolote.jpg" height="400px">
-    </div>
-  </div>
-</content>
 `;
