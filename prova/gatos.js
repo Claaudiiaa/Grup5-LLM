00893bfb39966153caa44e64a307768c34e5ce56
BitterKass <ariacasellas@inspladelestany.cat>
2023-05-11 15:52:58 +0200

    penjant prova del js amb la api

--- prova/gatos.js
+++ prova/gatos.js
@@ -1,24 +1,49 @@
-const content = document.getElementById('input');
+const selectDiv = document.getElementById('prova');
+const content = document.getElementById('content');
+const apiKey = '&api_key=live_QQWpmJhqpdymyzFNsookBeZbfMbKtFUW5qZOsoPtoAsWAKmzFComC6WXHxLkg2kx';
+const apiImages = 'https://api.thecatapi.com/v1/images/search?';
+const apiContent = 'https://api.thecatapi.com/v1/breeds';
 
+fetch('https://api.thecatapi.com/v1/breeds')
+    .then(response => response.json())
+    .then(data => {
+        const select = document.createElement('select');
+        let gatId, nom, imgId;
+        data.forEach(gat => {
+            const option = document.createElement('option');
+            option.value = gat.id;
+            option.textContent = gat.name;
+            option.setAttribute('data-img-id', gat.reference_image_id);
+            select.appendChild(option);
+        });
+        selectDiv.appendChild(select);
 
-fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => {
-            const select = document.createElement('select');
-            let nom;
-            data.forEach(user => {
-                  const option = document.createElement('option');
-                  option.value = user.id;
-                  option.textContent = user.name;
-                  select.appendChild(option);
-                  nom = user.name;
+        select.addEventListener('change', function () {
+            const selectedOption = this.options[this.selectedIndex];
+            gatId = selectedOption.value;
+            nom = selectedOption.textContent;
+            imgId = selectedOption.getAttribute('data-img-id');
+            buscar(imgId, nom, gatId);
+        });
+    });
+
+function buscar(imgId, nom, gatId) {
+    fetch(apiContent)
+        .then(response => response.json())
+        .then(json => {
+            const filtered = json.filter(cat => cat.id == gatId);
+            content.innerHTML = '';
+            filtered.forEach(cat => {
+                const item = document.createElement('li');
+                item.textContent = `Cat ID: ${gatId} Cat Name: ${nom} Cat Origin: ${cat.origin} Desc: ${cat.description}`;
+                content.appendChild(item);
             });
-            div.appendChild(select);
-
-            select.addEventListener('change', function () {
-                  const userId = this.value;
-                  buscar(userId, nom);
-                  const photos = document.getElementById('photos');
-                  generarAlbum(userId, nom);
-            });
-      });
+        });
+    fetch(`${apiImages}${imgId}${apiKey}`)
+        .then(response => response.json())
+        .then(json => {
+            const item = document.createElement('img');
+            item.src = json[0].url;
+            content.appendChild(item);
+        });
+}
